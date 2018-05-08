5fab225231beda91356099cb915baff4c8891b2c
TiDox <info@tidox.de>
2018-05-08 12:40:51 +0200

    MoneySaver - Implementierung Infrastruktur

---
+++ app/src/main/java/tidox/de/moneysaver/MainActivity.java
@@ -0,0 +1,13 @@
+package tidox.de.moneysaver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
